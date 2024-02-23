d510a8068863327520bc0b4e6d38da9dc48243f4
prashanth <reddyprashanth512@gmail.com>
2024-02-23 05:33:23 +0000

    solution

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+import Home from "./components/Home";
+import LoginForm from "./components/LoginForm";
+import "./App.css";
+
+const App = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/login" component={LoginForm} />
+      <Route exact path="/" component={Home} />
+    </Switch>
+  </BrowserRouter>
+);
+
+export default App;
